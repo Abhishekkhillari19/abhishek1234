cb4fbed2379c942fe96abf55a41427d1b054f14f
Abhishekkhillari19 <138864258+Abhishekkhillari19@users.noreply.github.com>
2023-07-17 15:00:43 +0530

    Update demo.java

--- demo.java
+++ demo.java
@@ -1,6 +1,5 @@
-Class demo {
-  public static void main(Strings[]args)
-    {
-    System.out.println("hello world");
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("Hello World");
   }
 }
